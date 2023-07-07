16e5d96bab57c5a1dbc39748e23a3b4a130063c1
MD <89563334+muhamaddigdaya@users.noreply.github.com>
2023-07-07 17:10:23 +0700

    font size

--- src/app/components/Text.tsx
+++ src/app/components/Text.tsx
@@ -6,7 +6,7 @@ type Props = {}
 const Text = (props: Props) => {
   return (
     <div className="hero-section flex flex-col xl:items-start sm:items-center p-8 w-screen ">
-        <header className='cursor-default font-extrabold sm:text-7xl xl:text-8xl xl:text-left sm:text-center mb-4'>Percentage Calculator</header>
+        <header className='cursor-default font-extrabold sm:text-5xl xl:text-8xl xl:text-left sm:text-center mb-4'>Percentage Calculator</header>
         <p className='cursor-default sm:text-md xl:text-xl w-fit bg-white p-2 border-2 border-black xl:shadow-inherit lg:shadow-[10px_10px_0px_0px_#000] hover:shadow-[10px_10px_0px_0px_#000] duration-150 ease-out'>A tool that might be useful for you.</p>
     </div>
   )
