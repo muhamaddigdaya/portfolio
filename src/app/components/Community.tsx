dab638fb2b7219ca8f00e2ffc167ffabd0c2c789
muhamaddigdaya <89563334+muhamaddigdaya@users.noreply.github.com>
2023-07-14 10:57:04 +0700

    next/link

--- src/app/components/Community.tsx
+++ src/app/components/Community.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 import React from "react"
 
 type Props = {}
@@ -8,48 +9,48 @@ const Community = (props: Props) => {
     <div className="flex sm:flex-col min-h-screen w-screen bg-[#A8A6FF] min-w-screen items-center justify-items-center justify-center p-8">
       <header className="cursor-default font-extrabold sm:text-4xl xl:text-8xl xl:p-16 lg:p-4 md:p-4 sm:p-4 text-white hover:text-[#FFF59F] duration-300 ease-out">Figma Community</header>
       <div className="grid xl:grid-cols-3 sm:grid-cols-none gap-8 text-black">
-      <a href="https://www.figma.com/community/file/1207145809192122717" target="_blank">
+      <Link href="https://www.figma.com/community/file/1207145809192122717" target="_blank">
         <div className="bg-white hover:bg-green-100 w-fit text-black p-4 space-y-2 border-4 w-fit border-black">
           <Image src="/Carousel.png" width={1920} height={960} alt="Carousel Thumbnail" className="xl:w-500px lg:w-450 md:w-400 sm:w-200px"/>
           <h2 className="text-xl font-bold text-white bg-[#FF5959] w-fit p-2">Interactive Carousel</h2>
           <p className="font-mono w-4/5">You can try the prototype now, go to the page, interact, and customize!</p>
         </div>
-      </a>
-      <a href="https://www.figma.com/community/file/1123140672040727667/UI-Component---Interactive-Dropdown-(customizable)" target="_blank">
+      </Link>
+      <Link href="https://www.figma.com/community/file/1123140672040727667/UI-Component---Interactive-Dropdown-(customizable)" target="_blank">
         <div className="bg-white hover:bg-orange-100 w-fit text-black p-4 space-y-2 border-4 w-fit border-black">
           <Image src="/Dropdown.png" width={1920} height={960} alt="Dropdown Thumbnail" className="xl:w-500px lg:w-450 md:w-400 sm:w-200px"/>
           <h2 className="text-xl font-bold text-white bg-[#308FFF] w-fit p-2">Interactive Dropdown</h2>
           <p className="font-mono w-4/5">Customize your own version of <b>Dropdown</b> without starting from scratch.</p>
         </div>
-      </a>
-      <a href="https://www.figma.com/community/file/1131738974378358873/UI-Component---Interactive-Selection-Radio-Button(customizable)" target="_blank">
+      </Link>
+      <Link href="https://www.figma.com/community/file/1131738974378358873/UI-Component---Interactive-Selection-Radio-Button(customizable)" target="_blank">
         <div className="bg-white hover:bg-blue-100 w-fit text-black p-4 space-y-2 border-4 w-fit border-black">
           <Image src="/Radio.png" width={1920} height={960} alt="Radio Thumbnail" className="xl:w-500px lg:w-450 md:w-400 sm:w-200px"/>
           <h2 className="text-xl font-bold text-black bg-[#FFA149] w-fit p-2">Interactive Radio Button</h2>
           <p className="font-mono w-4/5">Radio button pre-made for you to customize and match your project's style.</p>
         </div>
-      </a>
-      <a href="https://www.figma.com/community/file/1206095085125985582/Website-Landing-Page-Template-(neo-brutalism)" target="_blank">
+      </Link>
+      <Link href="https://www.figma.com/community/file/1206095085125985582/Website-Landing-Page-Template-(neo-brutalism)" target="_blank">
         <div className="bg-white hover:bg-yellow-100 w-fit text-black p-4 space-y-2 border-4 w-fit border-black">
           <Image src="/Website.png" width={1920} height={960} alt="Website Thumbnail" className="xl:w-500px lg:w-450 md:w-400 sm:w-200px"/>
           <h2 className="text-xl font-bold text-white bg-[#9747FF] w-fit p-2">Website Template</h2>
           <p className="font-mono w-4/5">Landing Page to inspire your next neo-brutalism project, or maybe even use it as a template.</p>
         </div>
-        </a>
-      <a href="https://www.figma.com/community/file/1209364886079568569/UI-Component---Interactive-Checkbox-(customizable)" target="_blank">
+        </Link>
+      <Link href="https://www.figma.com/community/file/1209364886079568569/UI-Component---Interactive-Checkbox-(customizable)" target="_blank">
         <div className="bg-white hover:bg-green-100 w-fit text-black p-4 space-y-2 border-4 w-fit border-black">
           <Image src="/Checkbox.png" width={1920} height={960} alt="Checkbox Thumbnail" className="xl:w-500px lg:w-450 md:w-400 sm:w-200px"/>
           <h2 className="text-xl font-bold text-black bg-[#FFA6F6] w-fit p-2">Interactive Checkbox</h2>
           <p className="font-mono w-4/5">Use this if you are lazy to make a checkbox. All done for you.</p>
         </div>
-      </a>
-      <a href="https://www.figma.com/community/file/1209140581871190509/UI-Component---Interactive-Button-(customizable)" target="_blank">
+      </Link>
+      <Link href="https://www.figma.com/community/file/1209140581871190509/UI-Component---Interactive-Button-(customizable)" target="_blank">
         <div className="bg-white hover:bg-purple-100  w-fit text-black p-4 space-y-2 border-4 w-fit border-black">
           <Image src="/Button.png" width={1920} height={960} alt="Button Thumbnail" className="xl:w-500px lg:w-450 md:w-400 sm:w-200px"/>
           <h2 className="text-xl font-bold text-black bg-[#FFF6A6] w-fit p-2">Interactive Button</h2>
           <p className="font-mono w-4/5">Completed button interactions, made for you, ready to use and customize.</p>
         </div>
-      </a>
+      </Link>
 
       </div>
     </div>
--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,15 +8,15 @@ const Footer = (props: Props) => {
   return (
     <div className='flex flex-row justify-between space-x-4 items-center w-screen bg-green-300 xl:p-8 lg:p-4 md:p-4 sm:p-2'>
         <div className='flex flex-row p-2 space-x-4 items-center '>
-          <a href="https://www.linkedin.com/in/muhamaddigdaya/" target='_blank'>
+          <Link href="https://www.linkedin.com/in/muhamaddigdaya/" target='_blank'>
             <svg xmlns="http://www.w3.org/2000/svg" className='fill-[#24292F] xl:h-[32px] lg:h-[32px] md:h-[32px] sm:h-[16px] hover:scale-[1.2]' viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
-          </a>
+          </Link>
           <Link href="https://www.github.com/muhamaddigdaya" target='_blank'>
             <Image src="/github-mark.png" width={34} height={34} alt="GitHub Logo" className='xl:h-[34px] lg:h-[34px] md:h-[34px] sm:h-[18px] hover:scale-[1.2]'/>
           </Link>
-          <a href="mailto:kamadanendra@gmail.com">
+          <Link href="mailto:kamadanendra@gmail.com">
             <svg xmlns="http://www.w3.org/2000/svg" className='fill-[#24292F] xl:h-[40px] lg:h-[40px] md:h-[40px] sm:h-[16px] hover:scale-[1.2]' viewBox="0 0 24 24"><path d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z"/></svg>
-          </a>
+          </Link>
         </div>
         <div>
             <Image src="/NOSHADOWSMILE.png" width={120} height={120} alt="smile :)" className='xl:w-[72px] lg:w-[72px] md:w-[72px] sm:w-[40px] animate-spin'/>
