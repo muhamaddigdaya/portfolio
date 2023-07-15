0533c3ef0c2141e728adcf97e53d3862bd962613
muhamaddigdaya <89563334+muhamaddigdaya@users.noreply.github.com>
2023-07-15 12:23:22 +0700

    suspense

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,16 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { Suspense } from 'react'
+import Loading from './Loading'
 
 type Props = {}
 
 const Header = (props: Props) => {
   return (
     <div className="hero-section flex flex-col xl:items-start sm:items-center w-screen">
-        <Image src="/SMILE.png" alt="SMILE!" width={120} height={120} className='animate-bounce'/>
+        <Suspense fallback={<Loading />}>
+          <Image src="/SMILE.png" alt="SMILE!" width={120} height={120} className='animate-bounce'/>
+        </Suspense>
         <header className='cursor-default font-extrabold sm:text-7xl xl:text-8xl sm:text-center mb-4'>Muhamad Digdaya</header>
         <p className='cursor-default sm:text-md xl:text-xl w-fit bg-white p-2 border-2 border-black xl:shadow-inherit lg:shadow-[10px_10px_0px_0px_#000] hover:shadow-[10px_10px_0px_0px_#000] duration-150 ease-out'>Inspiring over 3.5k designers trough <Link href="https://www.figma.com/@muhamaddigdaya" className='underline hover:font-bold' target='_blank'>Figma Community</Link></p>
     </div>
