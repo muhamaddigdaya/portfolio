a0d4bf3253f7a05c29963241068f48cfa983fc7f
MD <kamadanendra@gmail.com>
2023-10-19 11:39:26 +0700

    responsive fix

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,11 +7,11 @@ type Props = {}
 const Header = (props: Props) => {
   return (
     <div className="hero-section flex flex-col xl:items-start sm:items-center w-screen">
-        <Suspense fallback={`loading...`}>
-          <Image src="/SMILE.png" alt="SMILE!" width={120} height={120} className='animate-bounce'/>
-        </Suspense>
-        <header className='cursor-default font-extrabold sm:text-7xl xl:text-8xl sm:text-center mb-4'>Muhamad Digdaya</header>
-        <p className='cursor-default sm:text-md xl:text-xl w-fit bg-white p-2 border-2 border-black xl:shadow-inherit lg:shadow-[10px_10px_0px_0px_#000] hover:shadow-[10px_10px_0px_0px_#000] duration-150 ease-out'>Inspiring over 3.5k designers trough <Link href="https://www.figma.com/@muhamaddigdaya" className='underline hover:font-bold' target='_blank'>Figma Community</Link></p>
+      <Suspense fallback={`loading...`}>
+        <Image src="/SMILE.png" alt="SMILE!" width={120} height={120} className='animate-bounce' />
+      </Suspense>
+      <header className='cursor-default font-extrabold sm:text-7xl xl:text-8xl sm:text-center mb-4'>Muhamad Digdaya</header>
+      <p className='cursor-default text-sm xl:text-xl w-fit bg-white p-2 m-2 md:m-0 border-2 sm:max-w-full md:max-w-xl border-black xl:shadow-inherit lg:shadow-[10px_10px_0px_0px_#000] hover:shadow-[10px_10px_0px_0px_#000] duration-150 ease-out'>Inspiring over 4k designers trough <Link href="https://www.figma.com/@muhamaddigdaya" className='underline hover:font-bold' target='_blank'>Figma Community</Link></p>
     </div>
   )
 }
